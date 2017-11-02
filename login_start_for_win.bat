43b2661b2fe7977594db2726649859ca44301110
wanglelecc <wanglelecc@gmail.com>
2017-11-02 20:21:23 +0800

    添加web服务

--- login_start_for_win.bat
+++ login_start_for_win.bat
@@ -1,2 +1,2 @@
-php Applications\Login\start_register.php Applications\Login\start_gateway.php Applications\Login\start_businessworker.php
+php Applications\Login\start_register.php Applications\Login\start_web.php.php Applications\Login\start_gateway.php Applications\Login\start_businessworker.php
 pause
